91c051748a7ca115191610c8de5917e8bd45770e
Julien Simonet <j.mons54@gmail.com>
2019-11-10 08:35:45 +0000

    Exemples de commentaires

---
+++ src/javaHelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package javaHelloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//Un commentaire
+
+		/*
+		  Un autre commentaire
+		  sur plusiers ligne
+		 */
+	}
+}
